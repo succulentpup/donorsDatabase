9fe3361cf2b0afa4fc66911ccad66cac44f0c418
Ganesh Adapa <36997100+succulentpup@users.noreply.github.com>
2021-05-07 21:53:19 +0100

    initial commit, donors-simple-service skeleton has been implemented

---
+++ src/sample.ts
@@ -0,0 +1,3 @@
+// this file and function is created to demo unit tests, refer to sample.test.ts
+// todo: delete this file
+export const sum = (first: number, second: number): number => first + second;
---
+++ todo.md
@@ -0,0 +1,1 @@
+1. Edit codebuild policy to grant only required permissons on specified resources.
